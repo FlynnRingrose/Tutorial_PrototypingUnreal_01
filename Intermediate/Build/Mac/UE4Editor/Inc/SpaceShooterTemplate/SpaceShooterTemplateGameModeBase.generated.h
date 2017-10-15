d760fbad4b80d942204b6f1447423bbacd1f2ad2
Flynn Ringrose <flynnringrose@gmail.com>
2017-10-15 12:49:44 -0300

    SST40 - Player movement, projectiles, and death set up.

---
+++ Intermediate/Build/Mac/UE4Editor/Inc/SpaceShooterTemplate/SpaceShooterTemplateGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef SPACESHOOTERTEMPLATE_SpaceShooterTemplateGameModeBase_generated_h
+#error "SpaceShooterTemplateGameModeBase.generated.h already included, missing '#pragma once' in SpaceShooterTemplateGameModeBase.h"
+#endif
+#define SPACESHOOTERTEMPLATE_SpaceShooterTemplateGameModeBase_generated_h
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_RPC_WRAPPERS
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesASpaceShooterTemplateGameModeBase(); \
+	friend SPACESHOOTERTEMPLATE_API class UClass* Z_Construct_UClass_ASpaceShooterTemplateGameModeBase(); \
+public: \
+	DECLARE_CLASS(ASpaceShooterTemplateGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SpaceShooterTemplate"), NO_API) \
+	DECLARE_SERIALIZER(ASpaceShooterTemplateGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesASpaceShooterTemplateGameModeBase(); \
+	friend SPACESHOOTERTEMPLATE_API class UClass* Z_Construct_UClass_ASpaceShooterTemplateGameModeBase(); \
+public: \
+	DECLARE_CLASS(ASpaceShooterTemplateGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/SpaceShooterTemplate"), NO_API) \
+	DECLARE_SERIALIZER(ASpaceShooterTemplateGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASpaceShooterTemplateGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASpaceShooterTemplateGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpaceShooterTemplateGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpaceShooterTemplateGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpaceShooterTemplateGameModeBase(ASpaceShooterTemplateGameModeBase&&); \
+	NO_API ASpaceShooterTemplateGameModeBase(const ASpaceShooterTemplateGameModeBase&); \
+public:
+
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ASpaceShooterTemplateGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ASpaceShooterTemplateGameModeBase(ASpaceShooterTemplateGameModeBase&&); \
+	NO_API ASpaceShooterTemplateGameModeBase(const ASpaceShooterTemplateGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ASpaceShooterTemplateGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ASpaceShooterTemplateGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ASpaceShooterTemplateGameModeBase)
+
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_12_PROLOG
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_RPC_WRAPPERS \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_INCLASS \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID SpaceShooterTemplate_Source_SpaceShooterTemplate_SpaceShooterTemplateGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
